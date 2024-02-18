d83277b4cf97f71181282608d2e3110c673c3841
Tobias Bulai <42584575+TobiasBulai@users.noreply.github.com>
2024-02-18 14:23:53 +0100

    New progressbar config handling

--- progressbar.js
+++ progressbar.js
@@ -15,9 +15,16 @@ const StepConfig = class {
 }
 
 const ProgressBar = class {
-  element = undefined // HTMLElement
-  text = undefined // HTMLElement
-  loader = undefined // HTMLElement
+  /** @type {HTMLElement|undefined} */
+  element = undefined
+
+  /** @type {HTMLElement|undefined} */
+  text = undefined
+
+  /** @type {HTMLElement|undefined} */
+  loader = undefined
+
+  /** @type {object} */
   conf = {}
 
   /**
@@ -26,25 +33,41 @@ const ProgressBar = class {
    * @param {ProgressBarConfig|object} conf
    */
   constructor(element, conf = {}) {
-    try {
-      this.validate(element, conf)
+    this.validate(element, conf)
+    this.validateProgressBarConfiguration(conf)
 
-      this.element = element
+    this.element = element
 
-      this.text = document.createElement('div')
-      this.text.classList.add('progressbar', 'text')
-      this.text.style.display = 'none'
-      this.element.append(this.text)
+    this.text = document.createElement('div')
+    this.text.classList.add('progressbar', 'text')
+    this.text.style.display = 'none'
+    this.element.append(this.text)
 
-      this.loader = document.createElement('div')
-      this.loader.classList.add('progressbar', 'loader')
+    this.loader = document.createElement('div')
+    this.loader.classList.add('progressbar', 'loader')
 
-      this.configureProgressBar(conf)
+    this.configureProgressBar(conf)
 
-      this.element.append(this.loader)
-    } catch (error) {
-      console.error(error)
-    }
+    this.element.append(this.loader)
+  }
+
+  /**
+   *
+   * @param {ProgressBarConfig|object} conf
+   */
+  validateProgressBarConfiguration (conf) {
+    const propertyList = Object.getOwnPropertyNames(new ProgressBarConfig())
+    const suppliedConfigPropertyList = Object.getOwnPropertyNames(conf)
+
+    suppliedConfigPropertyList.forEach(prop => {
+      const foundProperty = propertyList.filter((propName) => {
+        return (propName === prop)
+      })
+
+      if (foundProperty === undefined) {
+        throw new Error(`Supplied property ${prop} is not supported!`)
+      }
+    })
   }
 
   /**
@@ -52,38 +75,50 @@ const ProgressBar = class {
    * @param {ProgressBarConfig|object} conf
    */
   configureProgressBar (conf) {
-    if (conf.initText !== undefined) {
-      this.text.style.display = 'table'
-      this.text.innerText = conf.initText
-    }
+    const propertyList = Object.getOwnPropertyNames(conf)
 
-    if (conf.width !== undefined) {
-      this.element.style.setProperty('--default-width', `${conf.width}px`)
-    }
+    propertyList.forEach(prop => {
+      switch (prop) {
+        case 'initText':
+          if (typeof conf[prop] !== 'string') {
+            this.createError(prop)
+          } else {
+            this.text.style.display = 'table'
+            this.text.innerText = conf.initText
+          }
 
-    if (conf.height !== undefined) {
-      this.element.style.setProperty('--default-height', `${conf.height}px`)
-    }
+          break;
+        case 'borderColor':
+        case 'progressColor':
+          if (typeof conf[prop] !== 'string') {
+            this.createError(prop)
+          } else {
+            const cssName = '--default-' + prop.replaceAll(/[A-Z]/g, (match) => {
+              return `-${match.toLowerCase()}`
+            })
+            this.element.style.setProperty(cssName, `${conf[prop]}`)
+          }
 
-    if (conf.borderWidth !== undefined) {
-      this.element.style.setProperty('--default-border-width', `${conf.borderWidth}px`)
-    }
+          break;
+        case 'width':
+        case 'height':
+        case 'borderWidth':
+        case 'padding':
+        case 'radius':
+          if (typeof conf[prop] !== 'number') {
+            this.createError(prop)
+          } else {
+            const cssName = '--default-' + prop.replaceAll(/[A-Z]/g, (match) => {
+              return `-${match.toLowerCase()}`
+            })
+            this.element.style.setProperty(cssName, `${conf[prop]}px`)
+          }
 
-    if (conf.borderColor !== undefined) {
-      this.element.style.setProperty('--default-border-color', conf.borderColor)
-    }
-
-    if (conf.padding !== undefined) {
-      this.element.style.setProperty('--default-padding', `${conf.padding}px`)
-    }
-
-    if (conf.radius !== undefined) {
-      this.element.style.setProperty('--default-radius', `${conf.radius}px`)
-    }
-
-    if (conf.progressColor !== undefined) {
-      this.element.style.setProperty('--default-progress-color', conf.progressColor)
-    }
+          break;
+        default:
+          throw new Error(`Supplied property '${prop}' of configuration does not exist!`)
+      }
+    })
   }
 
   /**
@@ -137,5 +172,9 @@ const ProgressBar = class {
     if (conf === undefined || typeof conf !== 'object') {
       throw new Error(`configuration is not an object!`)
     }
+  }
+
+  createError (configuration) {
+    throw new Error(`'${configuration}' supplied a not valid value`)
   }
 }
